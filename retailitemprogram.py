68cb668b0d1e02378d0e2b59b3de45aec7609b0d
ryandelongchamps <89552468+ryandelongchamps@users.noreply.github.com>
2021-09-24 22:50:07 -0500

    Add files via upload

---
+++ retailitemprogram.py
@@ -0,0 +1,17 @@
+import retailitemclass as r
+
+desc1 = 'Jacket'
+unit1 = '12'
+price1 = '59.95'
+desc2 = 'Designer Jeans'
+unit2 = '40'
+price2 = '34.95'
+desc3 = 'Shirt'
+unit3 = '20'
+price3 = '24.95'
+
+item1 = r.RetailItem(desc1,unit1,price1)
+item2 = r.RetailItem(desc2,unit2,price2)
+item3 = r.RetailItem(desc3,unit3,price3)
+
+print(item1.get_price)
